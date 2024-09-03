492fa32c01df3a8e45dcc384aa5044dfd7caaf7a
Alif Miftah Fauzan <aiipfauzan500@gmail.com>
2024-09-03 14:35:57 +0700

    web

--- routes/web.php
+++ routes/web.php
@@ -50,8 +50,7 @@ Route::delete('/admin-delete/{id}',[TambahController::class, 'delet'])->name('de
 
 Route::get('/siswa-dashboard', [SiswaController::class, 'index'])->name('siswa.dashboard');
 Route::get('/guru-dashboard', [GuruController::class, 'index'])->name('guru.dashboard');
-<<<<<<< HEAD
-=======
+
 Route::get('/guru-materi', [GuruController::class, 'materi'])->name('guru.materi');
 Route::get('/guru-jadwal', [GuruController::class, 'jadwal'])->name('guru.jadwal');
 Route::get('/guru-tugas', [GuruController::class, 'tugas'])->name('guru.tugas');
@@ -59,12 +58,18 @@ Route::get('/guru-profil', [GuruController::class, 'profil'])->name('guru.profil
 Route::get('/guru-addMateri', [GuruController::class, 'addMateri'])->name('guru.addMateri');
 Route::get('/guru-addTugas', [GuruController::class, 'addTugas'])->name('guru.addTugas');
 
+//siswa
+Route::get('/siswa-materi', [GuruController::class, 'materi'])->name('siswa.materi');
+Route::get('/siswa-jadwal', [GuruController::class, 'jadwal'])->name('siswa.jadwal');
+Route::get('/siswa-tugas', [GuruController::class, 'tugas'])->name('siswa.tugas');
+Route::get('/siswa-profil', [GuruController::class, 'profil'])->name('siswa.profil');
+Route::get('/guru-addTugas', [GuruController::class, 'addTugas'])->name('guru.addTugas');
+
 Route::get('/admin/tambah_siswa', [TambahTugasController::class, 'tambah_siswa'])->name('tambah_siswa');
 Route::post('/tambahsiswa', [TambahTugasController::class, 'create'])->name('create_siswa');
 
 
 //Orang Tua
->>>>>>> a1c72692189fc0dd979147507f78e882666299e2
 Route::get('/orangtua-dashboard', [OrangTuaController::class, 'index'])->name('orangtua.dashboard');  
 
 
