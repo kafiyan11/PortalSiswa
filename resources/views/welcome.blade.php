4a0d49c59d152d53f9b96969efbaa66ccd68965b
kafiyan11 <kafiyanenjoy@gmailcom>
2024-09-10 16:26:31 +0700

    mengubah judul

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title>Landing Page</title>
+  <title>Portal Siswa SMKN 1 Kawali</title>
   <meta name="description" content="">
   <meta name="keywords" content="">
 
