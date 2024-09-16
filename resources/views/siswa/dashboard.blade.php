4b332d8f10d55f4a61493b1fabce11b918fd9c61
Alif Miftah Fauzan <aiipfauzan500@gmail.com>
2024-09-16 17:43:58 +0700

    percobaan

--- resources/views/siswa/dashboard.blade.php
+++ resources/views/siswa/dashboard.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>percobaan</title>
+    <title>Daftar Jadwal</title>
     <!-- Link ke Bootstrap CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <!-- Link ke CSS custom (jika ada) -->
